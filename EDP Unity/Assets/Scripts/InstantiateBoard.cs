aca5c83c23ad16608686ed4cefcff08b1abd28a6
hassanhardy <hassanhardy@hotmail.com>
2019-03-15 16:24:18 -0400

    Board Moving with Trigger

--- EDP Unity/Assets/Scripts/InstantiateBoard.cs
+++ EDP Unity/Assets/Scripts/InstantiateBoard.cs
@@ -7,18 +7,18 @@ public class InstantiateBoard : MonoBehaviour
 
     public GameObject MainGame;
     public Transform Trigger;
+    private GameObject MainGameInstance;
     // Start is called before the first frame update
     void Start()
     {
-        
-            Instantiate(MainGame, Trigger.position, Trigger.rotation);
-        
-        
+        MainGameInstance = Instantiate(MainGame, Trigger.position, Trigger.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        MainGameInstance.transform.position = Trigger.transform.position;
+        MainGameInstance.transform.rotation = Trigger.transform.rotation;
         
     }
 }
