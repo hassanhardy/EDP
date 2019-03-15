d6072c22b34bfb3ec3d07d305a15fcad08cf0868
hassanhardy <hassanhardy@hotmail.com>
2019-03-15 16:08:10 -0400

    Trigger Instantiation

---
+++ EDP Unity/Assets/Scripts/InstantiateBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBoard : MonoBehaviour
+{
+
+    public GameObject MainGame;
+    public Transform Trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+            Instantiate(MainGame, Trigger.position, Trigger.rotation);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
